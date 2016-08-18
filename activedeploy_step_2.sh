96e8d8a64d6673adf78dc1b650344a39d08f865c
BHeubach <bheubach@de.ibm.com>
2016-08-18 16:15:33 +0200

    issue 127 merging logs to Osthanes stage1 code wi https://github.ibm.com/ActiveDeploy/team/issues/127

--- activedeploy_step_2.sh
+++ activedeploy_step_2.sh
@@ -107,7 +107,9 @@ if [[ ${TEST_RESULT_FOR_AD} -eq 0 ]]; then
     logError "Unable to advance to final phase"
   fi
 else
+  logInfo "TEST_RESULT_FOR_AD is: ${TEST_RESULT_FOR_AD}"
   logInfo "Test failure -- rolling back update ${update_id}"
+  logInfo "After rollback, Active Deploy Complete job will exit with failure."
   rollback ${update_id} && rc=$? || rc=$?
   if (( $rc )); then
     logInfo "$(wait_comment $rc)"
